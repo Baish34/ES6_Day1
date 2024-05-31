b43a95480f524d939bd67431e5ae27fdcee00ef8
Baishnawi Agrawal <145450500+Baish34@users.noreply.github.com>
2024-05-31 05:51:36 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,62 @@
+// console.log("Program 1:")
+// let age = 20
+// console.log("Original age:", age)
+// age = 22
+// console.log("Updated age:", age)
+
+// console.log("Program 2:")
+// let num1 = 20
+// let num2 = 5
+
+// let product = num1 * num2
+// console.log("Product of num1 and num2:", product)
+
+// let difference = num1 - num2
+// console.log("Difference bewteen num1 and num2:", difference)
+
+// const numberOfGates = 5
+// numberOfGates = 6
+
+// console.log("Program 4:")
+// const obj = {
+//   name: "Elina",
+//   age: 30
+// }
+// console.log("Original person:", obj)
+// obj.age = 31
+// console.log("Updated person:", obj)
+
+// let colors = {
+//     primary: "red",
+//     secondary: "blue"
+// };
+
+// console.log("Original Object:", colors)
+// colors.tertiary = "green";
+
+// console.log("Updated Object:", colors);
+
+// const numbers = [1, 2, 3, 4]
+// numbers[2] = 99
+// console.log(numbers)
+
+// const coordinates = { x: 10, y: 20 }
+// console.log(coordinates)
+// const newCoordinates = { x: 30, y: 40 }
+// coordinates = newCoordinates
+
+// let obj = { a: 1, b: 2 }
+// const obj2 = { a: 3, b: 4 }
+// obj = obj2
+// console.log(obj)
+// console.log(obj2)
+
+// const numericVar = 2
+// numericVar = 3
+// console.log(numericVar)
+
+// const greeting = "Hello"
+// console.log(greeting)
+// let name = " Jay"
+// name = " Shawn"
+// console.log(greeting + name)
